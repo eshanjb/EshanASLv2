213decb273dae12d871b8641be19e349ea5164ad
kunalbhayana <kunal.bhayana@gmail.com>
2024-02-28 14:13:01 +0530

    v1

---
+++ pages/index.js
@@ -0,0 +1,265 @@
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import { drawHand } from "../components/handposeutil";
+import * as fp from "fingerpose";
+import Handsigns from "../components/handsigns";
+import debounce from "lodash/debounce";
+import {
+  Text,
+  Heading,
+  Button,
+  Image,
+  Stack,
+  Container,
+  Box,
+  VStack,
+  ChakraProvider,
+} from "@chakra-ui/react";
+
+import { Signimage, Signpass } from "../components/handimage";
+
+import About from "../components/about";
+import Metatags from "../components/metatags";
+
+import { RiCameraFill, RiCameraOffFill } from "react-icons/ri";
+
+export default function Home() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const [camState, setCamState] = useState("on");
+  const [detectedLetters, setDetectedLetters] = useState([]); // Track detected letters
+  const [sign, setSign] = useState(null);
+
+  let gamestate = "started";
+
+  async function runHandpose() {
+    const net = await handpose.load();
+
+    setInterval(() => {
+      detect(net);
+    }, 150);
+  }
+
+  async function detect(net) {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const hand = await net.estimateHands(video);
+
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.ThumbsUpGesture,
+          Handsigns.aSign,
+          Handsigns.bSign,
+          Handsigns.cSign,
+          Handsigns.dSign,
+          Handsigns.eSign,
+          Handsigns.fSign,
+          Handsigns.gSign,
+          Handsigns.hSign,
+          Handsigns.iSign,
+          Handsigns.jSign,
+          Handsigns.kSign,
+          Handsigns.lSign,
+          Handsigns.mSign,
+          Handsigns.nSign,
+          Handsigns.oSign,
+          Handsigns.pSign,
+          Handsigns.qSign,
+          Handsigns.rSign,
+          Handsigns.sSign,
+          Handsigns.tSign,
+          Handsigns.uSign,
+          Handsigns.vSign,
+          Handsigns.wSign,
+          Handsigns.xSign,
+          Handsigns.ySign,
+          Handsigns.zSign,
+        ]);
+
+        const estimatedGestures = await GE.estimate(hand[0].landmarks, 6.5);
+
+        if (gamestate === "played") {
+          document.querySelector("#app-title").innerText =
+            "Make a 👍 gesture with your hand to start";
+        }
+
+        if (
+          estimatedGestures.gestures !== undefined &&
+          estimatedGestures.gestures.length > 0
+        ) {
+          const confidence = estimatedGestures.gestures.map((p) => p.confidence);
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(undefined, confidence)
+          );
+
+          const detectedLetter = estimatedGestures.gestures[maxConfidence].name;
+
+          setSign(detectedLetter);
+
+          // Append the detected letter to the array without repetition
+          setDetectedLetters((prevLetters) => {
+            if (!prevLetters.includes(detectedLetter)) {
+              return [...prevLetters, detectedLetter];
+            }
+            return prevLetters;
+          });
+        }
+      }
+
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  }
+
+  // Function to speak the detected letters
+  function speakDetectedLetters() {
+    const textToSpeak = detectedLetters.join("");
+    if (window.speechSynthesis) {
+      const utterance = new SpeechSynthesisUtterance(textToSpeak);
+      window.speechSynthesis.speak(utterance);
+    }
+  }
+
+  // Function to clear detected letters
+  function clearDetectedLetters() {
+    setDetectedLetters([]);
+  }
+
+  useEffect(() => {
+    runHandpose();
+  }, []);
+
+  function turnOffCamera() {
+    if (camState === "on") {
+      setCamState("off");
+    } else {
+      setCamState("on");
+    }
+  }
+
+  return (
+    <ChakraProvider>
+      <Metatags />
+      <Box bgColor="#5784BA">
+        <Container centerContent maxW="xl" height="100vh" pt="0" pb="0">
+          <VStack spacing={4} align="center">
+            <Box h="20px"></Box>
+            <Heading
+              as="h3"
+              size="md"
+              className="tutor-text"
+              color="white"
+              textAlign="center"
+            ></Heading>
+            <Box h="20px"></Box>
+          </VStack>
+
+          <Heading
+            as="h1"
+            size="lg"
+            id="app-title"
+            color="white"
+            textAlign="center"
+          >
+            🧙‍♀️ Loading the Magic 🧙‍♂️
+          </Heading>
+
+          <Box id="webcam-container">
+            {camState === "on" ? (
+              <Webcam id="webcam" ref={webcamRef} />
+            ) : (
+              <div id="webcam" background="black"></div>
+            )}
+
+            {sign ? (
+              <div
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  right: "calc(50% - 50px)",
+                  bottom: 100,
+                  textAlign: "-webkit-center",
+                }}
+              >
+                <Text color="white" fontSize="sm" mb={1}>
+                  detected gestures
+                </Text>
+                <img
+                  alt="signImage"
+                  src={
+                    Signimage[sign]?.src
+                      ? Signimage[sign].src
+                      : "/loveyou_emoji.svg"
+                  }
+                  style={{
+                    height: 30,
+                  }}
+                />
+              </div>
+            ) : (
+              " "
+            )}
+          </Box>
+
+          <canvas id="gesture-canvas" ref={canvasRef} style={{}} />
+        </Container>
+        <Box
+          id="singmoji"
+          style={{
+            zIndex: 9,
+            position: "fixed",
+            top: "50px",
+            right: "30px",
+          }}
+        >
+          <Text color="white" fontSize="sm" mb={1}>
+            Detected Gestures:
+          </Text>
+          <Text color="white" fontSize="md">
+            {detectedLetters.join(" ")}
+          </Text>
+        </Box>
+
+        <Stack id="start-button" spacing={4} direction="row" align="center">
+          <Button
+            leftIcon={
+              camState === "on" ? (
+                <RiCameraFill size={20} />
+              ) : (
+                <RiCameraOffFill size={20} />
+              )
+            }
+            onClick={turnOffCamera}
+            colorScheme="orange"
+          >
+            Camera
+          </Button>
+          <Button onClick={speakDetectedLetters} colorScheme="blue">
+            Speak
+          </Button>
+          <Button onClick={clearDetectedLetters} colorScheme="red">
+            Clear
+          </Button>
+          <About />
+        </Stack>
+      </Box>
+    </ChakraProvider>
+  );
+}
